f326d51182e020aa9a748ccacba7790372f0f521
alechaito <alechaito@gmail.com>
2023-01-01 15:36:17 +1000

    feat: max heapify algorithm

---
+++ datastructures/heapsort/max_heapify.py
@@ -0,0 +1,35 @@
+A = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+
+def right(i):
+    return 2*i + 1
+
+def left(i):
+    return 2*i
+
+def max_heapify(A, i):
+    l = left(i)
+    r = right(i)
+    largest = None
+    tmp_A = None
+
+    if l <= len(A) and A[l-1] > A[i-1]:
+        largest = l
+    else:
+        largest = i
+    
+    if r <= len(A) and A[r-1] > A[largest-1]:
+        largest = r
+    
+    if largest != i:
+        tmp_A = A[i-1]
+        A[i-1] = A[largest-1]
+        A[largest-1] = tmp_A
+        max_heapify(A, largest)
+    
+    return A
+
+print("Before heapify: ", A)
+
+A = max_heapify(A, 2)
+
+print("After heapify: ", A)
