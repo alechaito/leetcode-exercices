61b0b12f88ec2a22ed99e639bea879b3d0434b85
alechaito <alechaito@gmail.com>
2023-01-01 16:07:08 +1000

    feat: min heapify

--- datastructures/heapsort/max_heapify.py
+++ datastructures/heapsort/max_heapify.py
@@ -3,35 +3,35 @@
 A = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
 
 def right(i):
-    return 2*i + 1
+    return 2*i + 2
 
 def left(i):
-    return 2*i
+    return 2*i + 1
 
 def max_heapify(A, i):
     l = left(i)
     r = right(i)
     largest = None
-    tmp_A = None
 
-    if l <= len(A) and A[l-1] > A[i-1]:
+    if l < len(A) and A[l] > A[i]:
         largest = l
     else:
         largest = i
     
-    if r <= len(A) and A[r-1] > A[largest-1]:
+    if r < len(A) and A[r] > A[largest]:
         largest = r
     
     if largest != i:
-        tmp_A = A[i-1]
-        A[i-1] = A[largest-1]
-        A[largest-1] = tmp_A
+        A[i], A[largest] = A[largest], A[i]
         max_heapify(A, largest)
     
-    return A
 
 print("Before heapify: ", A)
 
-A = max_heapify(A, 2)
+
+n = int((len(A)//2)-1)
+for k in range(n, -1, -1):
+    max_heapify(A, k)
 
 print("After heapify: ", A)
+
---
+++ datastructures/heapsort/min_heapify.py
@@ -0,0 +1,36 @@
+# T(n) = O(lgn)
+
+A = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1]
+
+def right(i):
+    return 2*i + 2
+
+def left(i):
+    return 2*i + 1
+
+def min_heapify(A, i):
+    l = left(i)
+    r = right(i)
+    smallest = None
+
+    if l < len(A) and A[l] < A[i]:
+        smallest = l
+    else:
+        smallest = i
+    
+    if r < len(A) and A[r] < A[smallest]:
+        smallest = r
+    
+    if smallest != i:
+        A[i], A[smallest] = A[smallest], A[i]
+        min_heapify(A, smallest)
+    
+
+print("Before heapify: ", A)
+
+n = int((len(A)//2)-1)
+for k in range(n, -1, -1):
+    min_heapify(A, k)
+
+print("After heapify: ", A)
+
