8363726f095a24200801b9ab044ab425241ea1b6
alechaito <alechaito@gmail.com>
2023-01-05 10:07:17 +1000

    feat: find first palindromic word in array

---
+++ first_palindromic.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
+
+words = ["abc","car","ada","racecar","cool"]
+
+def find_first_palindromic_in_array(words: list) -> str:
+    for word in words:
+        palindromic = True
+        for i in range((len(word)+1)//2):
+            if word[i] != word[~i]:
+                palindromic = False
+                break
+        
+        # we found just return because its the first found
+        if palindromic:
+            return word
+    # if not found anything just return empty string
+    return ""
+
+# trick: use operator "~"
+
+# Time complexity: O(n)
+# Memory complexity: O(1)
